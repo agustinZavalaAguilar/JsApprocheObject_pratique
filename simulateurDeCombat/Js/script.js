db039075828b8a1a73b28edd56222ce15cc29ea7
agustinZavalaAguilar <88766456+agustinZavalaAguilar@users.noreply.github.com>
2024-01-01 13:02:12 +0100

    Experiments with DOM selectors and modification through Jquery

--- simulateurDeCombat/Js/script.js
+++ simulateurDeCombat/Js/script.js
@@ -46,4 +46,36 @@ let billySurvivor = new Personnage("billySurvivor", 50, 30,40);
 billySurvivor.attaquer(clicker);
 clicker.lancerAttaqueSpecial(billySurvivor);
 
+/*---------Experimentation DOM et pratique avec des fonctions JS et JsQuery----*/
+// 1) Fonction Js pour changer les noms des combatants--------------------------
+function updateFightersNames(survivorName,zombieName) {
+    var el = document.getElementById("nomDuSurvivant");
+    el.textContent = survivorName;
+    var el = document.getElementById("nomDuZombie");
+    el.textContent = zombieName;
+}
+//Appel de la fonction crée ci-dessus:
+//updateFightersNames('Tony Survivor', 'Hungry Petunia');
 
+// 2) Remaniement de la fonction ci-dessous pour effectuer la même action avec
+//    une expression de fonction:-----------------------------------------------
+
+let changeFightersNames = (survivorName,zombieName)=>{
+    var el = document.getElementById("nomDuSurvivant");
+    el.textContent = survivorName;
+    var el = document.getElementById("nomDuZombie");
+    el.textContent = zombieName;
+}
+//Appel de l'expression de fonction ci-dessous:
+//changeFightersNames('Cool Dog Survivor','Ugly Zombie Lady');
+
+// 3) JQuery pour effectuer la même fonction de changement des noms des combattants:--
+
+let renameFighters = (survivorName,zombieName)=>{
+    $('#nomDuSurvivant').text(survivorName);
+    
+    var el = document.getElementById("nomDuZombie");
+    el.textContent = zombieName;
+}
+
+renameFighters('Jquery Survivor', 'Old fashioned Zombie Lady');
